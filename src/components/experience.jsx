aed8d39e763055cdde7bf03811d2085c8f0295e8
Sharon Drury <sharon@sharondrury.co.uk>
2025-11-24 17:06:11 +0000

    changes to responsiveness

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,22 +6,26 @@ function Experience() {
       <div className="numbers-container">
         <div>
           <h3>Years as a contractor</h3>
+        </div>
+        <div>
+          <h3>Years IT experience</h3>
+        </div>
+        <div>
+          <h3>Qualifications</h3>
+        </div>
+        <div>
           <h2 className="counter">
             <CountUp start={0} end={15} />+
           </h2>
         </div>
         <div>
-          <h3>Years IT experience</h3>
           <h2 className="counter">
             <CountUp start={0} end={25} />+
           </h2>
         </div>
-        <div>
-          <h3>Qualifications</h3>
-          <h2 className="counter">
-            <CountUp start={0} end={5} />
-          </h2>
-        </div>
+        <h2 className="counter">
+          <CountUp start={0} end={5} />
+        </h2>
       </div>
     </>
   );
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,20 +1,24 @@
 function Hero() {
   return (
     <>
-    <div className="hero-section">
+      <div className="hero-section">
         <div className="hero-background">
-            <div className="hero-text">
-                <div className="hero-text-container">
-                    <div>
-                        <h1>Guy Salmon Consultancy</h1>
-                        <h2>Experienced infrastructure engineer</h2>
-                    </div>
-                    <div>
-                        <button className="left-btn">Get in touch</button>
-                        <button className="right-btn">Download cv</button>
-                    </div>
-                </div>
+          <div className="hero-text">
+            <div className="hero-text-container">
+              <div>
+                <h1>Guy Salmon Consultancy</h1>
+                <h2>Experienced infrastructure engineer</h2>
+              </div>
+              <div>
+                <a href="" className="hero-btn left-btn">
+                  Get in touch
+                </a>
+                <a href="" className="hero-btn right-btn">
+                  Download cv
+                </a>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </>
