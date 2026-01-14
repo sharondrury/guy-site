91a98891ef8c4b1a0bab5c0366effd7f0a352d30
Sharon Drury <sharon@sharondrury.co.uk>
2026-01-14 14:21:20 +0000

    removed + from year and updated scroll in view

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,52 +1,36 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 const experience = () => {
-  const [viewPortEntered, setViewPortEntered] = useState(false);
-  const containerRef = useRef(null);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setViewPortEntered(true);
-        }
-      },
-      {
-        threshold: 0.3, // Trigger when 30% of the component is visible
-      }
-    );
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    return () => {
-      if (containerRef.current) {
-        observer.unobserve(containerRef.current);
-      }
-    };
-  }, []);
+   const { ref, inView } = useInView({
+     triggerOnce: false, // Set to true if you want it to trigger only once
+     threshold: 0.3,
+   });
 
   return (
     <>
-      <div className="numbers-container" ref={containerRef}>
+      <div className="numbers-container" ref={ref}>
         <div className="column-section column-one">
           <h3>Company established in</h3>
           <h2 className="counter">
-            {viewPortEntered ? (
-              <CountUp start={2000} end={2013} duration={2} />
-            ) : (
-              0
+            {inView && (
+              <CountUp
+                start={2000}
+                end={2013}
+                duration={2}
+                separator=""
+                decimals={0}
+              />           
             )}
-            +
           </h2>
         </div>
 
         <div className="column-section column-two">
           <h3>Years IT experience</h3>
           <h2 className="counter">
-            {viewPortEntered ? <CountUp start={0} end={26} duration={2} /> : 0}+
+            {inView ? <CountUp start={0} end={26} duration={2} /> : 0}+
           </h2>
         </div>
       </div>
